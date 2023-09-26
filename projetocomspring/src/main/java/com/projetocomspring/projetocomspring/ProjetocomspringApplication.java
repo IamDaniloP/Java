4fee153e68007be81c8e53a891676bf6bb179119
Dido notebook <danilo.pp2610@gmail.com>
2023-09-26 11:23:43 -0300

    Iniciando projeto com spring boot

---
+++ projetocomspring/src/main/java/com/projetocomspring/projetocomspring/ProjetocomspringApplication.java
@@ -0,0 +1,13 @@
+package com.projetocomspring.projetocomspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocomspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocomspringApplication.class, args);
+	}
+
+}
---
+++ projetocomspring/src/test/java/com/projetocomspring/projetocomspring/ProjetocomspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocomspring.projetocomspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocomspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
